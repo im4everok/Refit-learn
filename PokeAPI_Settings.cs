b9280dd645aea22f967cf5bbef9365ae1a5c0f95
im4everok <imizyk09@gmail.com>
2024-03-17 20:33:58 +0200

    Add project files.

---
+++ PokeAPI_Settings.cs
@@ -0,0 +1,8 @@
+﻿namespace RefitLearn
+{
+    public class PokeAPI_Settings
+    {
+        public string GetLocationsEndpoint { get; set; }
+        public string GetBerriesEndpoint { get; set; }
+    }
+}
