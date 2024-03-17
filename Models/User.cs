a2197edc0a5d67b89bc345fa5116b8b1893fd88c
im4everok <imizyk09@gmail.com>
2024-03-17 21:18:05 +0200

    added user model

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace RefitLearn.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString() =>
+            string.Join(Environment.NewLine, $"Id: {Id}, Name: {Name}, Email: {Email}");
+    }
+}
