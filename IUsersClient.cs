39723576d99c409d49fdd9f75bce8756f5ec60d0
im4everok <imizyk09@gmail.com>
2024-03-17 21:18:17 +0200

    added refit users cient

---
+++ IUsersClient.cs
@@ -0,0 +1,23 @@
+﻿using Refit;
+using RefitLearn.Models;
+
+namespace RefitLearn
+{
+    public interface IUsersClient
+    {
+        [Get("/users")]
+        Task<IEnumerable<User>> GetUsers();
+
+        [Get("/users/{id}")]
+        Task<User> GetUser(int id);
+
+        [Post("/users")]
+        Task<User> CreateUser([Body] User user);
+
+        [Put("/users/{id}")]
+        Task<User> UpdateUser(int id, [Body] User user);
+
+        [Delete("/users/{id}")]
+        Task DeleteUser(int id);
+    }
+}
